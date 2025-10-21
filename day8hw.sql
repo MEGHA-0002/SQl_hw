cf52460c5bdb09129dde80e6a6f31105791943f2
MEGHA AJITH <meghaajith002@gmail.com>
2025-10-21 17:47:08 +0530

    Add files via upload

---
+++ day8hw.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE BookStoreDB;
+
+USE BookStoreDB;
+
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    country VARCHAR(100)
+);
+
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(150),
+    price DECIMAL(10,2),
+    author_id INT);
+
+ALTER TABLE books
+ADD published_year INT;
+
+TRUNCATE TABLE books;
+
+DROP DATABASE BookStoreDB;
