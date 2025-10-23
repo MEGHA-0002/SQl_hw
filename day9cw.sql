1385bb091f75f4a76e40e7bb49f0cce7e2dc4b27
MEGHA AJITH <meghaajith002@gmail.com>
2025-10-23 11:13:02 +0530

    Add files via upload

---
+++ day9cw.sql
@@ -0,0 +1,14 @@
+SELECT e.id, e.name, d.department_name
+FROM employees e
+LEFT JOIN departments d
+ON e.id = d.emp_id;
+
+SELECT e.id, e.name, d.department_name
+FROM employees e
+INNER JOIN departments d
+ON e.id = d.emp_id;
+
+SELECT d.emp_id, d.department_name, e.name
+FROM departments d
+LEFT JOIN employees e
+ON d.emp_id = e.id;
