de989082aef9c9e40665eb03b62ede8dce91d508
MEGHA AJITH <meghaajith002@gmail.com>
2025-10-23 11:33:11 +0530

    Reformat SQL queries for consistency

--- day9cw.sql
+++ day9cw.sql
@@ -3,12 +3,15 @@ FROM employees e
 LEFT JOIN departments d
 ON e.id = d.emp_id;
 
+
 SELECT e.id, e.name, d.department_name
 FROM employees e
 INNER JOIN departments d
 ON e.id = d.emp_id;
 
+
 SELECT d.emp_id, d.department_name, e.name
 FROM departments d
 LEFT JOIN employees e
 ON d.emp_id = e.id;
+
